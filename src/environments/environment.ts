7ff57ecaad5c168105f160158cdc3ec09bee241c
Andres-Uribe55 <omaruribe0609@gmail.com>
2026-02-09 15:32:24 -0500

    chore: Update API and hub URLs from localhost to a specific IP address in environment configurations.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:5269/api',
-    hubUrl: 'http://localhost:5269/hubs/order'
+    apiUrl: 'http://10.0.120.28:5269/api',
+    hubUrl: 'http://10.0.120.28:5269/hubs/order'
 };
